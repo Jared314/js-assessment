25245b2214ff75315455bbe66cf5ef52266831dd
Jared Lobberecht <pretender314@gmail.com>
2014-04-16 01:54:26 -0500

    fixing count

--- app/count.js
+++ app/count.js
@@ -3,6 +3,18 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+        var x = start,
+            interval = null;
+        
+        console.log(x);
+
+        interval = setInterval(function(){
+            x += 1;
+            console.log(x);
+            if(x === end) clearInterval(interval);
+        }, 100);
+        
+        return {'cancel': function(){ clearInterval(interval); }};
     }
   };
 });
