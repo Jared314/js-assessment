8310e94c957ffdf38ea589b98434d5d1878c494b
Jared Lobberecht <pretender314@gmail.com>
2014-04-16 02:47:36 -0500

    fixing async

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+        var d = $.Deferred();
+        d.resolve(value);
+        return d.promise();
     },
 
     manipulateRemoteData : function(url) {
+        var d = $.Deferred();
+        
+        $.get(url, function(data){
+            var people = data.people.map(function(x){ return x.name; });
+            people.sort();
+            d.resolve(people);
+        });
 
+        return d.promise();
     }
   };
 });
